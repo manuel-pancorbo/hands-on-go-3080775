67fd4e10e7c7b87b733025e17c83d2d08fd6d42a
Manuel Pancorbo <manuelpancorbo90@gmail.com>
2023-06-05 11:05:28 +0000

    challenge 2 implemented

--- challenges/types-primitive/begin/main.go
+++ challenges/types-primitive/begin/main.go
@@ -1,18 +1,31 @@
 // challenges/types-primitive/begin/main.go
 package main
 
+import "fmt"
+
 // use type inference to create a package-level variable "val" and assign it a value of "global"
+var val = "global"
 
 func main() {
 	// create a local variable "val" and assign it an int value
-
+	var val = 0
 	// print the value of the local variable "val"
-
+	fmt.Println(val)
 	// print the value of the package-level variable "val"
-
+	printGlobalVal()
 	// update the package-level variable "val" and print it again
-
+	updateGlobalVal("new-global")
 	// print the pointer address of the local variable "val"
-
+	fmt.Println(&val)
 	// assign a value directly to the pointer address of the local variable "val" and print the value of the local variable "val"
+	*(&val) = 1
+	fmt.Println(val)
+}
+
+func printGlobalVal() {
+	fmt.Println(val)
+}
+
+func updateGlobalVal(newVal string) {
+	val = newVal
 }
